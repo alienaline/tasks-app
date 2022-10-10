d8a53c0fb7391a36c8dcba11ddabb63db7610786
alienaline <alinafowork@gmail.com>
2022-10-10 20:52:09 +0300

    added reddux-persist

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from '../src/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from '../src/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,42 @@
 /* eslint-disable no-undef */
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { 
+    persistStore, 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import listsReducer from './listsSlice/listsSlice';
 import tasksReducer from './tasksSlice/tasksSlice';
 
-export default configureStore({
-    reducer: {
-        lists: listsReducer,
-        tasks: tasksReducer
-    }
+const rootReducer = combineReducers({
+    lists: listsReducer,
+    tasks: tasksReducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
+
